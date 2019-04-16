31f6a0aec8e09ad769747b12b590bae3b5d3e06a
Amy Cen <amy.cen@blackrock.com>
2019-04-16 06:29:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = 0
+  min_item = nil
+  name_hash.each do |item, num|
+    if num < min
+      min = num
+      min_item = item
+    end
+  end
+  min_item
 end
